0a4f380b9ccf154d8114d12dbb3904a32afc9181
ManhPD <manhpd3@vtn-global.com>
2026-04-14 09:17:04 +0700

    Release 1.2.11-26.4

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "FlowKit",
-      url: "https://github.com/mahainc/flow-kit/releases/download/1.2.11-26.1.1/FlowKit.xcframework.zip",
-      checksum: "96c037783d26b3467e917b3b06ad637065fb5f78c87f303285c50a7a308fd1d3"
+      url: "https://github.com/mahainc/flow-kit/releases/download/1.2.11-26.4/FlowKit.xcframework.zip",
+      checksum: "91ca419bfb29a34165779bd1b17d2954244f58e43ac1a1d397d74bef8aa5b2d4"
     ),
     .target(
       name: "CModules",
