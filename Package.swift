4aa6270467c8f35cf6740fd36fd06478419b4a5b
ManhPD <manhpd3@vtn-global.com>
2026-05-15 11:36:02 +0700

    Release 1.2.37-26.1.1-ffi

--- Package.swift
+++ Package.swift
@@ -5,14 +5,19 @@ let package = Package(
   name: "FlowKit",
   platforms: [.macOS(.v14), .iOS(.v15)],
   products: [
-    .library(name: "FlowKit", targets: ["FlowKit"]),
+    .library(name: "FlowKit", targets: ["FlowKit", "asyncify_wasmFFI"]),
     .library(name: "FlowKitCModules", targets: ["CModules"]),
   ],
   targets: [
     .binaryTarget(
       name: "FlowKit",
-      url: "https://github.com/mahainc/flow-kit/releases/download/1.2.37-26.1.1-wasmkit/FlowKit.xcframework.zip",
-      checksum: "43400b08890c80fd82f57f236729b23ffa4d669bd1bf63e60ad02654c220e32a"
+      url: "https://github.com/mahainc/flow-kit/releases/download/1.2.37-26.1.1-ffi/FlowKit.xcframework.zip",
+      checksum: "78aabffbe58f18e476003dc8b6282799f0c78bf9eeadf352cfe2bcba0d4c6622"
+    ),
+    .target(
+      name: "asyncify_wasmFFI",
+      path: "Sources/asyncify_wasmFFI",
+      publicHeadersPath: "."
     ),
     .target(
       name: "CModules",
