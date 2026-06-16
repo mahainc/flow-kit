6cdd48f9f20dd213ad01f5b1047352f3bef42884
ManhPD <manhpd3@vtn-global.com>
2026-06-16 14:44:33 +0700

    Release 1.2.56-26.1.1-wasmkit

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "FlowKit",
-      url: "https://github.com/mahainc/flow-kit/releases/download/1.2.55-26.1.1-wasmkit/FlowKit.xcframework.zip",
-      checksum: "3f29ce69dd51c0d4d9a7d3a924c1c2d83099870d9ca8e7537dba7e80f1c25371"
+      url: "https://github.com/mahainc/flow-kit/releases/download/1.2.56-26.1.1-wasmkit/FlowKit.xcframework.zip",
+      checksum: "7235b06d82d5f6f964f77a6e1311f36d0af780e80f772b0f851c165f4b05c234"
     ),
     .target(
       name: "CModules",
